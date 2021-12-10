6a253501a813aabe1e67cf19b9755911c8fa3334
sfianchao1022 <sfianchao@yahoo.com.tw>
2021-12-10 22:22:05 +0800

    add 1 month preparation kit week1

---
+++ oneMonthPreparationKit-week1/src/hackerrank/Result.java
@@ -0,0 +1,227 @@
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+    public static long repeatedString(String s, long n) {
+        // Write your code here
+        List<String> list = List.of(s.split(""));
+
+        int num = 0;
+        for (String str : list) {
+            if (str.equals("a")){
+                num = num + 1;
+            }
+        }
+        System.out.println(num);
+
+        List<String> leftList = new ArrayList<>();
+        long leftLength = n % (long) list.size();
+        if (leftLength != 0) {
+            leftList = list.subList(0, (int) leftLength);
+        }
+
+        int leftNum = 0;
+        for (String str : leftList) {
+            if (str.equals("a")){
+                leftNum = leftNum + 1;
+            }
+        }
+        System.out.println(leftNum);
+
+        Long ans = num * (n / (long) list.size()) + leftNum;
+
+        return ans;
+    }
+
+    /*
+     * Complete the 'jumpingOnClouds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY c as parameter.
+     */
+    public static int jumpingOnClouds(List<Integer> c) {
+        // Write your code here
+
+        List<Integer> list = new ArrayList<>();
+        int start = 0;
+        int skip = 0;
+        int ans = 0;
+        for (int i = 0; i < c.size(); i++) {
+            if (c.get(i) == 1) {
+                list.add(c.subList(start, i - 1).size());
+                skip = skip + 1;
+                start = i + 1;
+            }
+        }
+
+        list.add(c.subList(start, c.size() - 1).size());
+
+        for (Integer i : list) {
+            if (i % 2 == 0) {
+                ans = ans + (i / 2);
+            } else {
+                ans = ans + (i / 2) + 1;
+            }
+        }
+
+        return ans + skip;
+    }
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+        int seaLevel = 0;
+
+        int num = 0;
+        for (int i = 0; i < path.length(); i++) {
+            String s = String.valueOf(path.charAt(i));
+            if (s.equals("U")) {
+                seaLevel++;
+            } else {
+                seaLevel--;
+            }
+
+            // if we just came UP to sea level
+            if (seaLevel == 0 && s.equals("U")) {
+                num++;
+            }
+        }
+        return num;
+    }
+
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static void plusMinus(List<Integer> arr) {
+        // Write your code here
+        Map<String, Integer> map = new HashMap<>();
+        map.put("p", 0);
+        map.put("z", 0);
+        map.put("n", 0);
+        for (int i = 0; i < arr.size(); i++){
+            if (arr.get(i) > 0){
+                map.put("p", map.get("p") + 1);
+            } else if (arr.get(i) == 0){
+                map.put("z", map.get("z") + 1);
+            } else {
+                map.put("n", map.get("n") + 1);
+            }
+        }
+
+        float p = (float) (Math.round((map.get("p") / (float)arr.size()) * 1000000))/1000000;
+        float z = (float) (Math.round((map.get("z") / (float)arr.size()) * 1000000))/1000000;
+        float n = (float) (Math.round((map.get("n") / (float)arr.size()) * 1000000))/1000000;
+
+        System.out.println(p);
+        System.out.println(n);
+        System.out.println(z);
+
+        // another solution
+//        int pos = 0;
+//        int zero = 0;
+//        int neg = 0;
+//        for (int i = 0; i < arr.size(); i++) {
+//            int x = arr.get(i);
+//            if (x > 0) {
+//                pos++;
+//            } else if (x == 0) {
+//                zero++;
+//            } else {
+//                neg++;
+//            }
+//        }
+//        System.out.printf("%.6f\n", pos / (float) arr.size());
+//        System.out.printf("%.6f\n", neg / (float) arr.size());
+//        System.out.printf("%.6f\n", zero / (float) arr.size());
+
+    }
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+
+        List<Long> list = arr.stream().mapToLong(Integer::longValue).sorted().boxed().collect(Collectors.toList());
+        long min = list.get(0) + list.get(1) + list.get(2) + list.get(3);
+        long max = list.get(1) + list.get(2) + list.get(3) + list.get(4);
+
+//        // selection sort
+//        for (int i = 0; i < arr.size() - 1; i++){
+//            for (int j = i + 1; j < arr.size(); j++){
+//                if (arr.get(i) > arr.get(j)){
+//                    int temp = arr.get(j);
+//                    arr.set(j, arr.get(i));
+//                    arr.set(i, temp);
+//                }
+//            }
+//        }
+//        int min = arr.get(0) + arr.get(1) + arr.get(2) + arr.get(3);
+//        int max = arr.get(1) + arr.get(2) + arr.get(3) + arr.get(4);
+
+        System.out.println(min + " " + max);
+    }
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+    public static String timeConversion(String s) {
+        // Write your code here
+        int hour = Integer.parseInt(s.substring(0, 2));
+        String ampm = s.substring(s.length() - 2, s.length());
+
+        if (hour >= 12 && ampm.equals("AM")) {
+            hour = hour - 12;
+        }
+
+        if (hour < 12 && ampm.equals("PM")) {
+            hour = hour + 12;
+        }
+
+        String h = "";
+        if (hour < 10) {
+            h = "0" + String.valueOf(hour);
+        } else {
+            h = String.valueOf(hour);
+        }
+        s = h + s.substring(2, s.length() - 2);
+        System.out.println(s);
+        return s;
+    }
+
+
+}
---
+++ oneMonthPreparationKit-week1/src/hackerrank/Solution.java
@@ -0,0 +1,43 @@
+package hackerrank;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // warm up: repeatedString
+//        long result = Result.repeatedString("aba", 10);
+//        System.out.println(result);
+
+        // warm up: Jumping on the Clouds
+//        List<Integer> arr = Arrays.asList(0, 0, 1, 0, 0, 1, 0);
+//        int ans = Result.jumpingOnClouds(arr);
+//        System.out.println(ans);
+
+        // warm up: Counting Valleys
+//        int result = Result.countingValleys(12, "DDUUDDUDUUUD");
+//        System.out.println(result);
+
+        // 1. plusMinus
+//        List<Integer> arr = Arrays.asList(-4, 3, -9, 0, 4, 1);
+//        Result.plusMinus(arr);
+
+        // 2. Mini-Max Sum
+//        List<Integer> arr = Arrays.asList(1, 5, 3, 9, 7);
+//        Result.miniMaxSum(arr);
+
+        // 3. Time Conversion
+//        Result.timeConversion("07:05:45PM");
+
+        // 4.
+
+
+
+
+
+    }
+}
