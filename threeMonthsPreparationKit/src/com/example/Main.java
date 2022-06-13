7da68a286d7372374f691cc5dac065bb3d7ed96d
sfianchao <30313481+sfianchao@users.noreply.github.com>
2022-06-14 07:07:57 +0800

    threeMonths 2.

--- threeMonthsPreparationKit/src/com/example/Main.java
+++ threeMonthsPreparationKit/src/com/example/Main.java
@@ -8,7 +8,12 @@ public class Main {
 	// write your code here
 
         // 1. Breaking the Records
-        System.out.println(Result.breakingRecords(Arrays.asList(12, 24, 10, 24)));
+//        System.out.println(Result.breakingRecords(Arrays.asList(12, 24, 10, 24)));
+
+        // 2. Divisible Sum Pairs
+        System.out.println(Result.divisibleSumPairs(6, 3, Arrays.asList(1, 3, 2, 6, 1, 2)));
+
+
 
     }
 }
--- threeMonthsPreparationKit/src/com/example/Result.java
+++ threeMonthsPreparationKit/src/com/example/Result.java
@@ -35,5 +35,26 @@ public class Result {
         return Arrays.asList(max, min);
     }
 
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        // Write your code here
+        int pairs = 0;
+        for (int i = 0; i < ar.size(); i++) {
+            for (int j = i + 1; j < ar.size(); j++) {
+                if ((ar.get(i) + ar.get(j)) % k == 0) {
+                    pairs++;
+                }
+            }
+        }
+        return pairs;
+    }
 
 }
