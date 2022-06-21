f9b16eba7c321222c21339f05b1aa4de9d0adbcd
sfianchao <30313481+sfianchao@users.noreply.github.com>
2022-06-22 01:35:22 +0800

    threeMonths week4

--- threeMonthsPreparationKit/src/com/example/Main.java
+++ threeMonthsPreparationKit/src/com/example/Main.java
@@ -1,11 +1,22 @@
 package com.example;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        // Trend Micro: Pairs Sums
+//        System.out.println(Result.pairsSums(new int[]{5, 5, 5, 5, 5, 5}));
+
+        // Trend Micro: Pothole fixing
+        System.out.println();
+
+        // Trend Micro: Seat Reservation
+        System.out.println();
 
         // 1. Breaking the Records
 //        System.out.println(Result.breakingRecords(Arrays.asList(12, 24, 10, 24)));
@@ -17,7 +28,24 @@ public class Main {
 //        System.out.println(Result.marsExploration("SOSSPSSQSSOR"));
 
         // 4. Grading Students
-        System.out.println(Result.gradingStudents(Arrays.asList(4, 73, 67, 38, 33)));
+//        System.out.println(Result.gradingStudents(Arrays.asList(4, 73, 67, 38, 33)));
 
+        // 5. Migratory Birds
+//        System.out.println(Result.migratoryBirds(Arrays.asList(1, 1, 2, 2, 3)));
+
+        // 6. Subarray Division 2
+//        System.out.println(Result.birthday(Arrays.asList(1, 2, 1, 3, 2), 3, 2));
+
+        // 7. Picking Numbers
+//        System.out.println(Result.pickingNumbers(Arrays.asList(1, 1, 2, 2, 4, 4, 5, 5, 5)));
+
+        // 8. Left Rotation
+//        System.out.println(Result.rotateLeft(4, new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5))));
+
+        // 9. Number Line Jumps
+//        System.out.println(Result.kangaroo(0, 2, 5, 3));
+
+        // 10. Separate the Numbers
+        Result.separateNumbers("91011");
     }
 }
--- threeMonthsPreparationKit/src/com/example/Result.java
+++ threeMonthsPreparationKit/src/com/example/Result.java
@@ -1,11 +1,37 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Result {
+
+    /**
+     * Trend Micro: Pairs Sums
+     * @param A
+     * @return
+     */
+    public static int pairsSums(int[] A) {
+
+        int i = 1;
+        int count = 0;
+
+        while (i < A.length) {
+
+            if (i == A.length - 1) {
+                if ((A[i] + A[0]) % 2 == 0) {
+                    count++;
+                    break;
+                }
+            }
+
+            if ((A[i] + A[i - 1]) % 2 == 0) {
+                count++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        return count;
+    }
 
     /*
      * Complete the 'breakingRecords' function below.
@@ -104,5 +130,155 @@ public class Result {
         }
         return result;
     }
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<Integer> arr) {
+        // Write your code here
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        int type = 0;
+
+        for (Integer item : arr) {
+            map.put(item, map.getOrDefault(item, 0) + 1);
+        }
+
+        for (Integer key : map.keySet()) {
+
+            if (map.get(key) > max) {
+                max = map.get(key);
+                type = key;
+            } else if (map.get(key) == max && key < type) {
+                type = key;
+            }
+        }
+        return type;
+    }
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+    public static int birthday(List<Integer> s, int d, int m) {
+        // Write your code here
+        int i = 0;
+        int count = 0;
+        while (i <= s.size() - m) {
+            int sum = 0 ;
+            for (int j = i; j < i + m; j++) {
+                sum += s.get(j);
+            }
+            if (sum == d) {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+    public static int pickingNumbers(List<Integer> a) {
+        // Write your code here
+        int[] arr = new int[100];
+        int max = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            arr[a.get(i)]++;
+        }
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            max = Math.max(max, arr[i] + arr[i + 1]);
+        }
+
+        return max;
+
+    }
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        // Write your code here
+        for (int i = 0; i < d; i++) {
+            arr.add(arr.get(0));
+            arr.remove(0);
+        }
+        arr.forEach(System.out::println);
+
+        return arr;
+    }
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     */
+    public static String kangaroo(int x1, int v1, int x2, int v2) {
+        // Write your code here
+        int deltaX = Math.abs(x1 - x2);
+        int deltaV = Math.abs(v1 - v2);
+
+        if ((x1 >= x2 && v1 >= v2) || (x2 >= x1 && v2 >= v1)) {
+            return "NO";
+        } else if (deltaX % deltaV == 0) {
+            return "YES";
+        }
+        return "NO";
+    }
+
+    /*
+     * Complete the 'separateNumbers' function below.
+     *
+     * The function accepts STRING s as parameter.
+     */
+    public static void separateNumbers(String s) {
+        // Write your code here
+        String substring = "";
+        boolean isValid = false;
+        for (int i = 1; i <= s.length() / 2; i++) {
+            substring = s.substring(0, i);
+            Long num = Long.parseLong(substring);
+            String valid = substring;
+            while (valid.length() < s.length()) {
+                valid += String.valueOf(++num);
+            }
+            if (s.equals(valid)) {
+                isValid = true;
+                break;
+            }
+        }
+
+        if (isValid) {
+            System.out.println("YES " + substring);
+        } else {
+            System.out.println("NO");
+        }
+    }
+
 
 }
